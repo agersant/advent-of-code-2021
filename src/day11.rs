6564cc78addd92a34eb4d9535d7a45dd782ef80a
Antoine Gersant <antoine.gersant@lesforges.org>
2021-12-11 01:53:06 -0800

    Cleanup

--- src/day11.rs
+++ src/day11.rs
@@ -1,29 +1,27 @@
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 use std::{collections::HashSet, fs};
 
-fn read_input() -> Vec<Vec<u8>> {
+fn read_input() -> Vec<Vec<u32>> {
     fs::read_to_string("inputs/11")
         .unwrap()
         .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
-                .collect_vec()
-        })
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
         .collect_vec()
 }
 
-fn size(map: &Vec<Vec<u8>>) -> (usize, usize) {
+fn size(map: &Vec<Vec<u32>>) -> (usize, usize) {
     (map[0].len(), map.len())
 }
 
-fn neighbours(map: &mut Vec<Vec<u8>>, x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> {
+fn neighbours(map: &mut Vec<Vec<u32>>, x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> {
     let (w, h) = size(map);
-    (x.saturating_sub(1)..=(w - 1).min(x + 1))
-        .cartesian_product(y.saturating_sub(1)..=(h - 1).min(y + 1))
+    iproduct!(
+        x.saturating_sub(1)..=(w - 1).min(x + 1),
+        y.saturating_sub(1)..=(h - 1).min(y + 1)
+    )
 }
 
-fn step(map: &mut Vec<Vec<u8>>) -> usize {
+fn step(map: &mut Vec<Vec<u32>>) -> usize {
     let (w, h) = size(map);
     let mut flashed = HashSet::new();
     let mut todo = (0..w).cartesian_product(0..h).collect_vec();
